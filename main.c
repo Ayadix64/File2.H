3b2590d766ee73919bcebc8f9ae50061b3751944
Chamseddine <ayadi.chamseddine84@gmail.com>
2026-06-01 12:51:20 +0000

    fixing the library isuus, and making a append option

--- main.c
+++ main.c
@@ -1,42 +1,45 @@
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 typedef unsigned int u32;
 typedef unsigned long u64;
+typedef char bool;
+#define false 0
+#define true 1
 u64 max(u64 v1 , u64 v2){
 	return v1>v2?v1:v2;
 }
 u64 min(u64 v1 , u64 v2){
 	return v1>v2?v2:v1;
 }
-void* Push(char* val , u32 sizeofstr ,u64 pos,  u64& dataSize , u64& usedData , char* data){
+void* Push(char* val , u32 sizeofstr ,u64 pos,  u64* dataSize , u64* usedData , char* data){
 	
 	if(usedData+sizeofstr>= dataSize){
-		u64 newDataSize = dataSize+max(0x1000,usedData+sizeofstr);
+		u64 newDataSize = *dataSize+max(0x1000,*usedData+sizeofstr);
 		char* newdata = (char*)malloc(newDataSize);
-		memcpy(newdata, data, dataSize);
+		memcpy(newdata, data, *dataSize);
 		free(data);
 		data=newdata;
-		dataSize=newDataSize; 
+		*dataSize=newDataSize; 
 	}
-	if(pos>=dataSize){
-		u64 newDataSize = dataSize+max(0x1000,usedData+sizeofstr);
-		char* newdata = (char*)malloc(dataSize+max(0x1000,pos));
-		memcpy(newdata, data, dataSize);
+	if(pos>=*dataSize){
+		u64 newDataSize = *dataSize+max(0x1000,*usedData+sizeofstr);
+		char* newdata = (char*)malloc(*dataSize+max(0x1000,pos));
+		memcpy(newdata, data, *dataSize);
 		free(data);
 		data=newdata;
-		dataSize=newDataSize;
+		*dataSize=newDataSize;
 	}
-	for(u32 i = usedData+sizeofstr; i>pos+sizeofstr; i--){
+	for(u32 i = *usedData+sizeofstr; i>pos+sizeofstr; i--){
 		data[i-1] = data[i-sizeofstr-1];
 	}
 	
 	for(u32 i = 0 ; i < sizeofstr ; i++){
 		data[i+pos]=val[i];
 	}
-	usedData+=sizeofstr;
+	*usedData+=sizeofstr;
 	return data;
 }
 unsigned long GetFileSize(FILE* fl){
@@ -54,14 +57,14 @@ unsigned long GetFileSize(FILE* fl){
 	return fileSize;
 
 }
-void* readFile(const char* fileName , unsigned long & sizeOUT){
+void* readFile(const char* fileName , unsigned long * sizeOUT){
 	FILE* file = fopen(fileName, "r");
 	if(file==NULL){
 		printf("err in opening file\n");
 		return NULL;
 	}
 	unsigned long fileSz = GetFileSize(file);
-	sizeOUT=fileSz;
+	*sizeOUT=fileSz;
 	
 	if(fileSz!=-1){
 		char* data = (char*)malloc(fileSz);
@@ -76,8 +79,8 @@ void* readFile(const char* fileName , unsigned long & sizeOUT){
 	return NULL;
 }
 
-void WriteConstCharHaderFile(const char* haderName, char* matrixName , char* data , u64 sizeofData){
-	FILE* hader = fopen(haderName, "w");
+void WriteConstCharHaderFile(const char* haderName, char* matrixName , char* data , u64 sizeofData ,bool append ){
+	FILE* hader = fopen(haderName, append?"a":"w");
 	if(!hader){
 		printf("Erorr in writing hader file");
 		return;
@@ -109,7 +112,9 @@ void WriteConstCharHaderFile(const char* haderName, char* matrixName , char* dat
 	}
 	
 	
-
+	if(append){
+		fprintf(hader, "\n/********************************************%s***********************************************/\n",matrixName);
+	}
 	fprintf(hader, "const char* %s = \n",matrixName);
 	fwrite(data, 1, sizeofData-1,hader);
 	//fwrite(const void *__restrict ptr, size_t size, size_t n, FILE *__restrict s)
@@ -118,8 +123,8 @@ void WriteConstCharHaderFile(const char* haderName, char* matrixName , char* dat
 	return;
 }
 
-void WriteMatrixHaderFile(const char* haderName, char* matrixName , char* data , u64 sizeofData){
-	FILE* hader = fopen(haderName, "w");
+void WriteMatrixHaderFile(const char* haderName, char* matrixName , char* data , u64 sizeofData,bool append){
+	FILE* hader = fopen(haderName, append?"a":"w");
 	if(!hader){
 		printf("Erorr in writing hader file");
 		return;
@@ -150,7 +155,9 @@ void WriteMatrixHaderFile(const char* haderName, char* matrixName , char* data ,
 		matrixName[0]='_';
 	}
 	
-	
+	if(append){
+		fprintf(hader, "\n\n\n/********************************************%s***********************************************/\n",matrixName);
+	}
 
 	fprintf(hader, "const char %s[]{ \n\t",matrixName);
 	
@@ -174,8 +181,11 @@ int main(int argc, char** arg){
 		if( (arglng==2 & !memcmp(arg[1], "-h", 2)) || 
 		    (arglng==6 & !memcmp(arg[1], "--help", 6))
 		  ){
-			printf("Welcome to File2.H\nthis programe is under the MIT Licence\ncheckout the github : https://github.com/chamseddine2009-06/File2.H\n");
-			printf("===> [arg1 fileName] [arg2 2haderName] [arg3 StringName]\n-m at the end if you want to save it as matrix not as a string\n");
+			printf("Welcome to File2.H\nthis programe is under the MIT Licence\ncheckout the github : https://github.com/chamseddine2009-06/File2.H\n\n");
+			printf("===> [arg1 fileName] [arg2 2haderName] [arg3 StringName] -Opsion1 -Opsion2 ...\n"
+			       "\nOptions:\n"	
+			       "\t-m or --matrix if you want to save it as matrix not as a string.\n"
+			       "\t-a or --appand if you dont want to over-write the data in the Haderfile.\n");
 			return 0;
 		}
 	}
@@ -185,39 +195,56 @@ int main(int argc, char** arg){
 		return 1;
 	}
 	bool matrixmode = false;
-	if(strlen(arg[argc-1])==2 && !memcmp(arg[argc-1] ,"-m", 2)){
-		printf("\nYou are in matrix mod, all the data in your file will save a matrix not a string.\n");
-		matrixmode = true;
+	bool appendmode = false;
+	if(argc>3){
+		for(int i = 3 ; i < argc ; i++){
+			if(strlen(arg[i])==2 && !memcmp(arg[i] ,"-m", 2)){
+				printf("\nYou are in matrix mode, all the data in your file will save a matrix not a string.\n");
+				matrixmode = true;
+			}
+			if(strlen(arg[i])==2 && !memcmp(arg[i] ,"-a", 2)){
+				printf("\nYou are in appand mode.\n");
+				appendmode = true;
+			}
+			if(strlen(arg[i])==8 && !memcmp(arg[i] ,"--matrix", 8)){
+				printf("\nYou are in matrix mod, all the data in your file will save a matrix not a string.\n");
+				matrixmode = true;
+			}
+			if(strlen(arg[i])==8 && !memcmp(arg[i] ,"--append", 8)){
+				printf("\nYou are in appand mode.\n");
+				 matrixmode= true;
+			}
+		}
 	}
-	
+		
 	u64 dataSize=0;
-	char* data = (char*)readFile(arg[1], dataSize);
+	char* data = (char*)readFile(arg[1], &dataSize);
 	u64 usedData = dataSize;
 	printf("file %s size %d \n",arg[1],dataSize);
 	if(!matrixmode){
 		for(u64 i = dataSize ; i > 0 ; i--){
 			if(data[i-1] == '\n'){
-				data = (char*)Push("\t\"", 2, i, dataSize, usedData, data);
-				data = (char*)Push("\\n\"", 3, i-1, dataSize, usedData, data);
+				data = (char*)Push("\t\"", 2, i, &dataSize, &usedData, data);
+				data = (char*)Push("\\n\"", 3, i-1, &dataSize, &usedData, data);
 
 			}
 			if(data[i-1]=='"' ){
 				if(i>2 && data[i-2]=='\\' && data[i-3]!='\\'){
 					continue;
 				}
-				data = (char*)Push("\\", 1, i-1, dataSize, usedData, data);
+				data = (char*)Push("\\", 1, i-1, &dataSize, &usedData, data);
 			}
 		}
 		if(data[usedData-1]=='"'){
 			data[usedData-1]='\0';
 		}
 	
-		data=(char*)Push("\t\"", 2, 0, dataSize, usedData, data);
+		data=(char*)Push("\t\"", 2, 0, &dataSize, &usedData, data);
 		printf("Data size %d\n" , dataSize);
 		
-		WriteConstCharHaderFile(arg[2],arg[3], data, usedData);
+		WriteConstCharHaderFile(arg[2],arg[3], data, usedData,appendmode);
 	}else {
-		WriteMatrixHaderFile(arg[2], arg[3], data, dataSize);
+		WriteMatrixHaderFile(arg[2], arg[3], data, dataSize,appendmode);
 	}
 	
 	return 0;
